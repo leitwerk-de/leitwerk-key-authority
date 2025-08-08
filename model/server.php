3b3de8e41180182f602471b1bb4af4764d82875b
feweel <189344672+feweel@users.noreply.github.com>
2025-08-08 13:38:27 +0200

    fix hostname check

--- model/server.php
+++ model/server.php
@@ -624,7 +624,7 @@ class Server extends Record {
 				$output = $connection->exec('/bin/hostname -f');
 				$allowed_hostnames = array(trim($output));
 			}
-			if(!in_array($hostname, $allowed_hostnames)) {
+			if(!in_array($this->hostname, $allowed_hostnames)) {
 				throw new SSHException("Hostname check failed (allowed: ".implode(", ", $allowed_hostnames).").");
 			}
 		}
